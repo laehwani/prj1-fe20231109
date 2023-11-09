24de64be866c44ab1cab89356478274ea869ea41
hwani <klh481@naver.com>
2023-11-09 14:10:54 +0900

    응답 ok, internal server Error Status 작성함

--- src/page/BoardWrite.js
+++ src/page/BoardWrite.js
@@ -6,6 +6,7 @@ import {
   FormLabel,
   Input,
   Textarea,
+  useToast,
 } from "@chakra-ui/react";
 import axios from "axios";
 
@@ -13,6 +14,7 @@ export function BoardWrite() {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   const [writer, setWriter] = useState("");
+  const toast = useToast();
 
   function handleSubmit() {
     axios
@@ -21,8 +23,18 @@ export function BoardWrite() {
         content,
         writer,
       })
-      .then(() => console.log("잘됨!!!😊😊"))
-      .catch(() => console.log("안됨!!!😂😂"))
+      .then(() => {
+        toast({
+          description: "새 글이 저장되었습니다.",
+          status: "success",
+        });
+      })
+      .catch(() => {
+        toast({
+          description: "저장 중에 문제가 발생하였습니다.",
+          status: "error",
+        });
+      })
       .finally(() => console.log("끝!!😎😎"));
   }
 
