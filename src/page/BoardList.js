0ce79eb20a035cbf0924c0139ee71a2c85461cfc
hwani <klh481@naver.com>
2023-11-09 14:55:43 +0900

    BoardList 에서 게시물 목록 가져와서 보여주기 (axios)

--- src/page/BoardList.js
+++ src/page/BoardList.js
@@ -1,9 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Box, Th, Thead, Tr, Spinner, Td, Table } from "@chakra-ui/react";
+import axios from "axios";
 
 export function BoardList() {
+  const [boardList, setBoardList] = useState(null);
+
+  useEffect(() => {
+    axios.get("/api/board/list").then((r) => setBoardList(r.data));
+  }, []);
   return (
-    <div>
+    <Box>
       <h1>게시물 목록</h1>
-    </div>
+      <Box>
+        <Table>
+          <Thead>
+            <Tr>
+              <Th>id</Th>
+              <Th>title</Th>
+              <Th>by</Th>
+              <Th>at</Th>
+            </Tr>
+          </Thead>
+          <Thead></Thead>
+        </Table>
+        {boardList || <Spinner />}
+        {boardList &&
+          boardList.map((board) => (
+            <Tr>
+              <Td>{board.id}</Td>
+              <Td>{board.title}</Td>
+              <Td>{board.writer}</Td>
+              <Td>{board.inserted}</Td>
+            </Tr>
+          ))}
+      </Box>
+    </Box>
   );
 }
