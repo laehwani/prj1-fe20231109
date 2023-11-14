eea3e1a93588a2f3d337979c1aed57826cbd0a64
hwani <klh481@naver.com>
2023-11-14 16:54:43 +0900

    BoardEdit 취소 버튼 클릭시 이전경로로 이동

--- src/page/BoardEdit.js
+++ src/page/BoardEdit.js
@@ -1,6 +1,12 @@
-import {Box, FormControl, FormLabel, Input, Spinner} from "@chakra-ui/react";
+import {
+  Box, Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Spinner
+} from "@chakra-ui/react";
 import React, {useEffect} from "react";
-import {useParams} from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
 import {useImmer} from "use-immer";
 import axios from "axios";
 
@@ -10,6 +16,8 @@ export function BoardEdit() {
 
   // edit/:id
   const {id}= useParams();
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     axios
@@ -27,16 +35,33 @@ export function BoardEdit() {
         {id}번 글 수정
         <FormControl>
           <FormLabel>제목</FormLabel>
-          <Input value={board.title}/>
+          <Input value={board.title} onChange={e => {
+            updateBoard((draft) => {
+              draft.title = e.target.value
+            });
+          }}/>
         </FormControl>
         <FormControl>
           <FormLabel>본문</FormLabel>
-          <Input value={board.content}/>
+          <Input value={board.content} onChange={e => {
+            updateBoard((draft) => {
+              draft.content = e.target.value
+            });
+          }}/>
         </FormControl>
         <FormControl>
           <FormLabel>작성자</FormLabel>
-          <Input value={board.writer}/>
+          <Input value={board.writer} onChange={e => {
+            updateBoard((draft) => {
+              draft.writer = e.target.value
+            });
+          }}/>
         </FormControl>
+        <Button colorScheme={'yellow'}>저장</Button>
+
+        {/*navigate(-1) : 이전 경로로 이동, (-2) : 이전이전의 경로로 이동 */}
+        {/*navigate(+1) : 앞으로 이동*/}
+        <Button colorScheme={'blue'} onClick={()=>navigate(-1)}>취소</Button>
       </h1>
     </Box>
   </div>);
