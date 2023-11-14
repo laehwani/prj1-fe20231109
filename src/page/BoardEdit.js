11a355219026889b7d7beb773a2adad166b10e7a
hwani <klh481@naver.com>
2023-11-14 18:38:11 +0900

    BoardEdit 수정저장 시 확인모달창 띄우기

--- src/page/BoardEdit.js
+++ src/page/BoardEdit.js
@@ -1,5 +1,18 @@
 import {
-  Box, Button, FormControl, FormLabel, Input, Spinner, useToast
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Spinner, useDisclosure,
+  useToast
 } from "@chakra-ui/react";
 import React, {useEffect} from "react";
 import {useNavigate, useParams} from "react-router-dom";
@@ -17,6 +30,8 @@ export function BoardEdit() {
 
   const toast = useToast();
 
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   useEffect(() => {
     axios
     .get("/api/board/id/" + id)
@@ -27,7 +42,7 @@ export function BoardEdit() {
     return <Spinner/>;
   }
 
-  const handleSave = () => {
+  function handleSave (){
 
     // 저장 버튼 클릭 시
     // PUT /api/board/edit
@@ -42,51 +57,68 @@ export function BoardEdit() {
     .catch((error) => {
       if (error.response.data === 400) {
         toast({
-          description: "요청이 잘못되었습니다. 수정내용을 다시 확인해주세요!", status: 'error',
-        })
-      }
-      else {
+          description: "요청이 잘못되었습니다.수정내용을 다시 확인해주세요!",
+          status: "error",
+        });
+      } else {
         toast({
-          description: "수정 중에 문제가 발생하였습니다.", status: 'error',
+          description: "수정 중에 문제가 발생 하였습니다.",
+          status: "error",
         });
       }
     })
-    .finally(() => console.log('끝!'))
+    .finally(() => onClose());
   };
   return (<div>
     <Box>
       <h1>
-        {id}번 글 수정
-        <FormControl>
-          <FormLabel>제목</FormLabel>
-          <Input value={board.title} onChange={e => {
-            updateBoard((draft) => {
-              draft.title = e.target.value
-            });
-          }}/>
-        </FormControl>
-        <FormControl>
-          <FormLabel>본문</FormLabel>
-          <Input value={board.content} onChange={e => {
-            updateBoard((draft) => {
-              draft.content = e.target.value
-            });
-          }}/>
-        </FormControl>
-        <FormControl>
-          <FormLabel>작성자</FormLabel>
-          <Input value={board.writer} onChange={e => {
-            updateBoard((draft) => {
-              draft.writer = e.target.value
-            });
-          }}/>
-        </FormControl>
-        <Button colorScheme={'yellow'} onClick={handleSave}>저장</Button>
+        {id}번 글 수정</h1>
+      <FormControl>
+        <FormLabel>제목</FormLabel>
+        <Input value={board.title} onChange={e => {
+          updateBoard((draft) => {
+            draft.title = e.target.value
+          });
+        }}/>
+      </FormControl>
+      <FormControl>
+        <FormLabel>본문</FormLabel>
+        <Input value={board.content} onChange={e => {
+          updateBoard((draft) => {
+            draft.content = e.target.value
+          });
+        }}/>
+      </FormControl>
+      <FormControl>
+        <FormLabel>작성자</FormLabel>
+        <Input value={board.writer} onChange={e => {
+          updateBoard((draft) => {
+            draft.writer = e.target.value
+          });
+        }}/>
+      </FormControl>
+      <Button colorScheme={'yellow'} onClick={onOpen}>저장</Button>
 
-        {/*navigate(-1) : 이전 경로로 이동, (-2) : 이전이전의 경로로 이동 */}
-        {/*navigate(+1) : 앞으로 이동*/}
-        <Button colorScheme={'blue'} onClick={()=>navigate(-1)}>취소</Button>
-      </h1>
+      {/*navigate(-1) : 이전 경로로 이동, (-2) : 이전이전의 경로로 이동 */}
+      {/*navigate(+1) : 앞으로 이동*/}
+      <Button colorScheme={'blue'} onClick={()=>navigate(-1)}>취소</Button>
+
+      {/*수정 모달!*/}
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay/>
+        <ModalContent>
+          <ModalHeader>저장 확인</ModalHeader>
+          <ModalCloseButton/>
+          <ModalBody>저장 하시겠습니까?</ModalBody>
+
+          <ModalFooter>
+            <Button onClick={handleSave} colorScheme="blue">
+              저장
+            </Button>
+            <Button onClick={onClose}>취소</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Box>
   </div>);
 }
