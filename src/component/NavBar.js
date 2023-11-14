260a5a4ec617a73111c8da3f8ca18050b6341ebc
hwani <klh481@naver.com>
2023-11-14 23:28:07 +0900

    memberList 회원 목록 보기 화면 navbar 수정

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -3,13 +3,12 @@ import { useNavigate } from "react-router-dom";
 
 export function NavBar() {
   const navigate = useNavigate();
-  return (
-    <div>
-      <Flex>
-        <Button onClick={() => navigate("/")}>Home</Button>
-        <Button onClick={() => navigate("/write")}>Write</Button>
-        <Button onClick={()=> navigate("/signup")}>SignUp</Button>
-      </Flex>
-    </div>
-  );
+  return (<div>
+        <Flex>
+          <Button onClick={() => navigate("/")}>Home</Button>
+          <Button onClick={() => navigate("/write")}>Write</Button>
+          <Button onClick={() => navigate("/signup")}>SignUp</Button>
+          <Button onClick={() => navigate("/member/list")}>MemberList</Button>
+        </Flex>
+      </div>);
 }
