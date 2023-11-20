741fba70fdeb98a0291b2284a165e4e0fe64c461
hwani <klh481@naver.com>
2023-11-20 23:13:30 +0900

    프론트 댓글에 새로운 줄 출력.

--- src/component/CommentContainer.js
+++ src/component/CommentContainer.js
@@ -1,6 +1,17 @@
-import {Box, Button, Textarea} from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  CardBody,
+  CardHeader,
+  Heading,
+  Stack,
+  StackDivider,
+  Textarea,
+  Text,
+  Card, Flex
+} from "@chakra-ui/react";
 import axios from "axios";
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 
 function CommentForm({boardId}) {
 
@@ -8,8 +19,7 @@ function CommentForm({boardId}) {
 
   function handleSubmit() {
     axios.post("/api/comment/add", {
-      boardId,
-      comment
+      boardId, comment
     });
   }
 
@@ -23,23 +33,41 @@ function CommentForm({boardId}) {
 
 function CommentList({boardID}) {
 
-  const [commentList, setCommentList] = useState(null);
+  const [commentList, setCommentList] = useState([]);
 
   useEffect(() => {
     const params = new URLSearchParams();
     params.set("id", boardID);
 
-    axios
-    .get("/api/comment/list?"+ params)
+    axios.get("/api/comment/list?" + params)
     // TODO: urlsearchparams 로 url정보를 스트링화할때 + 가 왼쪽,오른쪽을
     // 모두 string으로 바꿔주기에 toString 을 안붙여도 된다..
-    .then(r=> setCommentList(r.data.value))
+    .then(r => setCommentList(r.data.value));
   }, []);
   return (<div>
-        <Box>
-          댓글 리스트
-        </Box>
-      </div>);
+    <Card>
+      <CardHeader>
+        <Heading size="md">댓글 리스트</Heading>
+      </CardHeader>
+      <CardBody>
+        <Stack divider={<StackDivider />} spacing="4">
+          {/*TODO: 새로운 줄 출력*/}
+          {/*TODO: 댓글 작성 후 re render*/}
+          {commentList.map((comment) => (
+              <Box>
+                <Flex justifyContent="space-between">
+                  <Heading size="xs">{comment.memberId}</Heading>
+                  <Text fontSize="xs">{comment.inserted}</Text>
+                </Flex>
+                <Text sx={{whiteSpace:"pre-wrap"}} pt="2" fontSize="sm">
+                  {comment.comment}
+                </Text>
+              </Box>
+          ))}
+        </Stack>
+      </CardBody>
+    </Card>
+  </div>);
 }
 
 export function CommentContainer({boardId}) {
