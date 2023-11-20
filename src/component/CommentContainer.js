0e483b1d5483b7e19a88edae6c2ebe3d91cfc836
hwani <klh481@naver.com>
2023-11-20 23:50:14 +0900

    프론트 댓글 쓰는 중 상태(isSubmitting) 을 CommentContainer 로 옮김..

--- src/component/CommentContainer.js
+++ src/component/CommentContainer.js
@@ -1,82 +1,96 @@
 import {
   Box,
   Button,
+  Card,
   CardBody,
   CardHeader,
+  Flex,
   Heading,
   Stack,
   StackDivider,
-  Textarea,
   Text,
-  Card, Flex
+  Textarea,
 } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 import axios from "axios";
-import React, {useEffect, useState} from "react";
 
-function CommentForm({boardId}) {
-
+function CommentForm({ boardId, isSubmitting, onSubmit }) {
   const [comment, setComment] = useState("");
 
   function handleSubmit() {
-    axios.post("/api/comment/add", {
-      boardId, comment
-    });
+    onSubmit({ boardId, comment });
   }
 
-  return (<div>
-        <Box>
-          <Textarea value={comment} onChange={e=> setComment(e.target.value)}></Textarea>
-          <Button onClick={handleSubmit}>댓글 쓰기</Button>
-        </Box>
-      </div>);
+  return (
+      <Box>
+        <Textarea value={comment} onChange={(e) => setComment(e.target.value)} />
+        <Button isDisabled={isSubmitting} onClick={handleSubmit}>
+          쓰기
+        </Button>
+      </Box>
+  );
 }
 
-function CommentList({boardID}) {
-
+function CommentList({ boardId }) {
   const [commentList, setCommentList] = useState([]);
 
   useEffect(() => {
     const params = new URLSearchParams();
-    params.set("id", boardID);
+    params.set("id", boardId);
 
-    axios.get("/api/comment/list?" + params)
+    axios
+    .get("/api/comment/list?" + params)
     // TODO: urlsearchparams 로 url정보를 스트링화할때 + 가 왼쪽,오른쪽을
-    // 모두 string으로 바꿔주기에 toString 을 안붙여도 된다..
-    .then(r => setCommentList(r.data.value));
+    //  모두 string으로 바꿔주기에 toString 을 안붙여도 된다..
+    .then((response) => setCommentList(response.data));
   }, []);
-  return (<div>
-    <Card>
-      <CardHeader>
-        <Heading size="md">댓글 리스트</Heading>
-      </CardHeader>
-      <CardBody>
-        <Stack divider={<StackDivider />} spacing="4">
-          {/*TODO: 새로운 줄 출력*/}
-          {/*TODO: 댓글 작성 후 re render*/}
-          {commentList.map((comment) => (
-              <Box>
-                <Flex justifyContent="space-between">
-                  <Heading size="xs">{comment.memberId}</Heading>
-                  <Text fontSize="xs">{comment.inserted}</Text>
-                </Flex>
-                <Text sx={{whiteSpace:"pre-wrap"}} pt="2" fontSize="sm">
-                  {comment.comment}
-                </Text>
-              </Box>
-          ))}
-        </Stack>
-      </CardBody>
-    </Card>
-  </div>);
+
+  return (
+      <Card>
+        <CardHeader>
+          <Heading size="md">댓글 리스트</Heading>
+        </CardHeader>
+        <CardBody>
+          <Stack divider={<StackDivider />} spacing="4">
+            {/* TODO: 댓글 작성 후 re render */}
+            {commentList.map((comment) => (
+                <Box>
+                  <Flex justifyContent="space-between">
+                    <Heading size="xs">{comment.memberId}</Heading>
+                    <Text fontSize="xs">{comment.inserted}</Text>
+                  </Flex>
+                  <Text sx={{ whiteSpace: "pre-wrap" }} pt="2" fontSize="sm">
+                    {/*TODO: sx={{whiteSpace:"pre-wrap"} 로 댓글에 새로운 줄 출력*/}
+                    
+                    {comment.comment}
+                  </Text>
+                </Box>
+            ))}
+          </Stack>
+        </CardBody>
+      </Card>
+  );
 }
 
-export function CommentContainer({boardId}) {
+export function CommentContainer({ boardId }) {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  function handleSubmit(comment) {
+    setIsSubmitting(true);
+
+    axios
+    .post("/api/comment/add", comment)
+    .finally(() => setIsSubmitting(false));
+  }
+
   return (
-      <div>
-        <Box>
-          <CommentForm boardId={boardId}></CommentForm>
-          <CommentList boardID={boardId}></CommentList>
-        </Box>
-      </div>
+      <Box>
+        <CommentForm
+            boardId={boardId}
+            isSubmitting={isSubmitting}
+            onSubmit={handleSubmit}
+        />
+        <CommentList boardId={boardId} />
+      </Box>
   );
 }
--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -49,3 +49,4 @@ export function NavBar() {
     </Flex>
   </div>);
 }
+
