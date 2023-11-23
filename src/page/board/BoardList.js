ca16d1269026f320d1088c1a3b65198f5b981fca
hwani <klh481@naver.com>
2023-11-23 15:02:04 +0900

    프론트 boardlist
    페이지 정보 얻기..임시..

--- src/page/board/BoardList.js
+++ src/page/board/BoardList.js
@@ -1,15 +1,16 @@
 import React, { useEffect, useState } from "react";
 import {
-  Box, Th, Thead, Tr, Spinner, Td, Table, Tbody, Badge,
+  Box, Th, Thead, Tr, Spinner, Td, Table, Tbody, Badge, Button,
 } from "@chakra-ui/react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import {useNavigate, useSearchParams} from "react-router-dom";
 import {ChatIcon} from "@chakra-ui/icons";
 import {faHeart} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 export function BoardList() {
   const [boardList, setBoardList] = useState(null);
+  const [pageInfo, setPageInfo] = useState(null);
 
   const [params] = useSearchParams();
 
@@ -18,8 +19,11 @@ export function BoardList() {
   useEffect(() => {
     axios
     .get("/api/board/list?" + params)
-    .then((response) => setBoardList(response.data));
-  }, []);
+    .then((response) => {
+      setBoardList(response.data.boardList);
+      setPageInfo(response.data.pageInfo);
+    });
+  }, [params]);
 
   if (boardList === null) {
     return <Spinner />;
@@ -68,6 +72,18 @@ export function BoardList() {
               ))}
             </Tbody>
           </Table>
+        </Box>
+        <Box>
+          <Button onClick={() => navigate("/?p=1")}>1</Button>
+          <Button onClick={() => navigate("/?p=2")}>2</Button>
+          <Button onClick={() => navigate("/?p=3")}>3</Button>
+          <Button onClick={() => navigate("/?p=4")}>4</Button>
+          <Button onClick={() => navigate("/?p=5")}>5</Button>
+          <Button onClick={() => navigate("/?p=6")}>6</Button>
+          <Button onClick={() => navigate("/?p=7")}>7</Button>
+          <Button onClick={() => navigate("/?p=8")}>8</Button>
+          <Button onClick={() => navigate("/?p=9")}>9</Button>
+          <Button onClick={() => navigate("/?p=10")}>10</Button>
         </Box>
       </Box>
   );
