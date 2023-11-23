6e8a7850f7a458eacf6838a4d35bfdd533ac4f0c
hwani <klh481@naver.com>
2023-11-23 16:21:35 +0900

    프론트 boardwrite
    파일 전송 기능 추가...

--- src/page/board/BoardWrite.js
+++ src/page/board/BoardWrite.js
@@ -14,6 +14,7 @@ import { useNavigate } from "react-router-dom";
 export function BoardWrite() {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
+  const [files, setFiles] = useState(null);
   const [isSubmitting, setIsSubmitting] = useState(false);
 
   const toast = useToast();
@@ -22,9 +23,10 @@ export function BoardWrite() {
   function handleSubmit() {
     setIsSubmitting(true);
     axios
-      .post("/api/board/add", {
+      .postForm("/api/board/add", {
         title,
         content,
+        files
       })
       .then(() => {
         toast({
@@ -64,6 +66,15 @@ export function BoardWrite() {
             value={content}
             onChange={(e) => setContent(e.target.value)}
           ></Textarea>
+        </FormControl>
+        <FormControl>
+          <FormLabel>이미지</FormLabel>
+          <Input
+              type="file"
+              accept="image/*"
+              multiple
+              onChange={(e) => setFiles(e.target.files)}
+          />
         </FormControl>
         <Button
           isDisabled={isSubmitting}
